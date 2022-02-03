7832b3585636339d80413abc20f66529d41fdedd
Mohamed Hassanin <mhassanin@microsoft.com>
2022-02-03 15:36:50 -0500

    PR updates 4

--- RetrospectiveExtension.Frontend/config/environment.tsx
+++
@@ -1,7 +0,0 @@
-const environment = {
-  CollaborationStateServiceUrl: 'https://app-retro-tool-24.azurewebsites.net', // change this to the deployed backend service
-  CurrentEnvironment: process.env.NODE_ENV,
-  AppInsightsInstrumentKey: 'c3f1611c-5060-4d9e-af29-2d781f104aa8' // put Instrumentation key here
-};
-
-export default environment;
