8baee0fac9921f4e15010d34c74360ba0081d260
Marisa Ashour <marisaashour@microsoft.com>
2022-02-07 13:11:00 -0500

    fix editable text

--- RetrospectiveExtension.Frontend/components/__tests__/editableText.test.tsx
+++ RetrospectiveExtension.Frontend/components/__tests__/editableText.test.tsx
@@ -3,11 +3,10 @@ import { shallow } from 'enzyme';
 import toJson from "enzyme-to-json";
 import EditableText, { EditableTextProps } from '../editableText';
 
-let testText = '';
 const mockOnSave = jest.fn(() => { });
 
-const defaultTestProps: EditableTextProps = {
-  text: testText,
+let mockedTestProps: EditableTextProps = {
+  text: '',
   isChangeEventRequired: false,
   onSave: mockOnSave
 }
@@ -15,15 +14,15 @@ const defaultTestProps: EditableTextProps = {
 describe('Editable Text Component', () => {
 
   it('renders correctly.', () => {
-    const wrapper = shallow(<EditableText {...defaultTestProps} />);
+    const wrapper = shallow(<EditableText {...mockedTestProps} />);
     const component = wrapper.children().dive();
     expect(toJson(component)).toMatchSnapshot();
   });
 
   it('updates text appropriately.', () => {
-    testText = 'Test Text';
+    mockedTestProps['text'] = 'Test Text';
 
-    const wrapper = shallow(<EditableText {...defaultTestProps} />);
+    const wrapper = shallow(<EditableText {...mockedTestProps} />);
     const component = wrapper.children().dive();
     expect(toJson(component)).toMatchSnapshot();
   });
