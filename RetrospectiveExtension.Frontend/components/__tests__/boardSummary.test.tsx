b32f19d362081a3967ed105ece2c67b536d150bd
Marisa Ashour <marisaashour@microsoft.com>
2022-02-09 10:36:29 -0500

    :hammer: add test for testing work item counts.

--- RetrospectiveExtension.Frontend/components/__tests__/boardSummary.test.tsx
+++ RetrospectiveExtension.Frontend/components/__tests__/boardSummary.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import BoardSummary, { IBoardSummaryProps } from '../boardSummary';
 import { WorkItem, WorkItemType } from '../__mocks__/azure-devops-extension-api/WorkItemTracking/WorkItemTracking';
 
@@ -13,20 +13,43 @@ const mockedDefaultProps: IBoardSummaryProps = {
   supportedWorkItemTypes: []
 };
 
+const mockedWorkItemCountProps: IBoardSummaryProps = {
+  actionItems: [],
+  isDataLoaded: false,
+  pendingWorkItemsCount: 2,
+  resolvedActionItemsCount: 3,
+  boardName: '',
+  feedbackItemsCount: 8,
+  supportedWorkItemTypes: []
+};
+
 describe('Board Summary', () => {
   test('renders with no action or work items.', () => {
     const wrapper = shallow(<BoardSummary {...mockedDefaultProps} />);
     const component = wrapper.children().dive();
 
-    console.log(component.debug());
+    verifySummaryBoardCounts(component, mockedDefaultProps);
+  });
 
-    expect(component.findWhere(
-      child => child.prop("aria-label") === "feedback item count").text()).toBe(0);
-    expect(component.findWhere(
-      child => child.prop("aria-label") === "total work items count").text()).toBe(0);
-    expect(component.findWhere(
-      child => child.prop("aria-label") === "pending work items count").text()).toBe(0);
-    expect(component.findWhere(
-      child => child.prop("aria-label") === "resolved work items count").text()).toBe(0);
+  test('renders with when work item counts are greater than zero.', () => {
+    const wrapper = shallow(<BoardSummary {...mockedWorkItemCountProps} />);
+    const component = wrapper.children().dive();
+
+    verifySummaryBoardCounts(component, mockedWorkItemCountProps);
   });
 });
+
+const verifySummaryBoardCounts = (component: ShallowWrapper, props: IBoardSummaryProps) => {
+  expect(component.findWhere(
+    child => child.prop("aria-label") === "feedback item count").text()).toBe(
+      props.feedbackItemsCount.toString());
+  expect(component.findWhere(
+    child => child.prop("aria-label") === "total work items count").text()).toBe(
+      (props.actionItems.length).toString());
+  expect(component.findWhere(
+    child => child.prop("aria-label") === "pending work items count").text()).toBe(
+      props.pendingWorkItemsCount.toString());
+  expect(component.findWhere(
+    child => child.prop("aria-label") === "resolved work items count").text()).toBe(
+      props.resolvedActionItemsCount.toString());
+};
