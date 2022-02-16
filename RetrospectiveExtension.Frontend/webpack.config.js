daa5f5916a8636b18fd043126ecc3b8b7980c170
Mohamed Hassanin <mhassanin@microsoft.com>
2022-02-16 13:02:15 -0500

    tip

--- RetrospectiveExtension.Frontend/webpack.config.js
+++ RetrospectiveExtension.Frontend/webpack.config.js
@@ -7,58 +7,70 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, '');
-
-module.exports = {
-  devtool: 'source-map',
-  entry: `${APP_DIR}/index.tsx`,
-  output: {
-    path: BUILD_DIR,
-    publicPath: './',
-    filename: './reflect-bundle.js',
-  },
-  resolve: {
-    fallback: {
-      assert: require.resolve('assert'),
-      buffer: require.resolve('buffer'),
-      crypto: require.resolve('crypto-browserify'),
-      process: 'process/browser',
-      stream: require.resolve('stream-browserify'),
-      util: require.resolve('util'),
+const mode = process.env.NODE_ENV || 'production';
+console.log(`mode is ${mode}`);
+console.log(`env var is ${process.env.NODE_ENV}`);
+module.exports = (env, argv) => {
+  // const mode = argv
+  return {
+    devtool: 'source-map',
+    entry: `${APP_DIR}/index.tsx`,
+    output: {
+      path: BUILD_DIR,
+      publicPath: './',
+      filename: './reflect-bundle.js',
     },
-    extensions: ['.ts', '.js', '.jsx', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts|.js|.tsx$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'ts-loader'
+    resolve: {
+      fallback: {
+        assert: require.resolve('assert'),
+        buffer: require.resolve('buffer'),
+        crypto: require.resolve('crypto-browserify'),
+        process: 'process/browser',
+        stream: require.resolve('stream-browserify'),
+        util: require.resolve('util'),
+      },
+      extensions: ['.ts', '.js', '.jsx', '.tsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts|.js|.tsx$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'ts-loader'
+          }
+        },
+        { test: /(\.css$)/, use: ['style-loader', 'css-loader'] },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+          use: 'url-loader?limit=100000'
         }
-      },
-      { test: /(\.css$)/, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-        use: 'url-loader?limit=100000'
-      }
+      ]
+    },
+    plugins: [
+      new MomentLocalesPlugin(),
+      new ESLintPlugin(),
+      new Dotenv(),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      new webpack.DefinePlugin({
+        'process.env.BUILD_BUILDNUMBER': JSON.stringify(process.env.BUILD_BUILDNUMBER)
+      }),
+
+      // new webpack.DefinePlugin({
+      //   'process.env': {
+      //     BUILD_BUILDNUMBER: JSON.stringify(process.env.BUILD_BUILDNUMBER),
+      //     'NODE_ENV': JSON.stringify(mode),
+      //   }
+      // })
     ]
-  },
-  plugins: [
-    new MomentLocalesPlugin(),
-    new ESLintPlugin(),
-    new Dotenv(),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        BUILD_BUILDNUMBER: JSON.stringify(process.env.BUILD_BUILDNUMBER),
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-      }
-    })
-  ]
+  };
 }
